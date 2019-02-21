0723a5943364b3b9c9cb53779beb95860f7a72e2
amersohail794 <amersohail794@gmail.com>
2019-02-21 17:49:04 +0400

    added different cases of date and time

--- reverseproxy.js
+++ reverseproxy.js
@@ -1,4 +1,9 @@
-const  logger = require('./winstonlogger')(__filename);
-const proxy = require('redbird')({port: process.env.PORT || 5000});
+// const  logger = require('./winstonlogger')(__filename);
+// const proxy = require('redbird')({port: process.env.PORT || 5000});
 
-proxy.register("https://obscure-harbor-15450.herokuapp.com", "http://94.200.209.170:10003");
+// proxy.register("https://obscure-harbor-15450.herokuapp.com", "http://94.200.209.170:10003");
+
+const  logger = require('./winstonlogger')(__filename);
+const proxy = require('redbird')({port: process.env.PORT || 9090});
+
+proxy.register("https://obscure-harbor-15450.herokuapp.com", "http://192.168.98.59:9090");
